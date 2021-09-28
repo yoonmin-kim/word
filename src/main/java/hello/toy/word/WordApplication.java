3aef1d98380ca2ac14a0c5189700c53060cc6eed
yoonmin-kim <rladbsals23@gmail.com>
2021-09-28 20:23:55 +0900

    docs : AutoConfiguration
    설정파일과 @Bean 정의 2가지 방식이 있음.
    
    [application.yml]
    spring:
      messages:
        basename: messages, test
        encoding: UTF-8
        cache-duration: 2
    
    [@Bean]
    반환타입으로 MessageSource를 설정하고
    ReloadableResourceBundleMessageSource를 생성하여 리턴한다.

--- src/main/java/hello/toy/word/WordApplication.java
+++ src/main/java/hello/toy/word/WordApplication.java
@@ -2,12 +2,25 @@ package hello.toy.word;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class WordApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WordApplication.class, args);
+	}
+
+	@Bean(name = "messageSource")
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setCacheSeconds(2);
+		return messageSource;
 	}
 
 }
